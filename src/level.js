8f826c8b298bd4d57efc50321b366e970954214e
Martijn van Iersel <mvaniersel@gmail.com>
2020-02-01 19:19:23 +0100

    Basic level progression, level win condition

---
+++ src/level.js
@@ -0,0 +1,54 @@
+export const levelData = [
+	// 0
+	{
+		w: 6, h: 4,
+		enemySpeedFactor: 0.8,
+	},
+	// 1
+	{
+		w: 7, h: 5,
+		enemySpeedFactor: 0.9,
+	},
+	// 2
+	{
+		w: 7, h: 6,
+		enemySpeedFactor: 1.0,
+	},
+	// 3
+	{
+		w: 6, h: 10,
+		enemySpeedFactor: 1.0,
+	},
+	// 4
+	{
+		w: 12, h: 6,
+		enemySpeedFactor: 1.1,
+	},
+	// 5
+	{
+		w: 10, h: 10,
+		enemySpeedFactor: 1.2,
+	},
+	// 6
+	{
+		w: 13, h: 9,
+		enemySpeedFactor: 1.25,
+	},
+	// 7
+	{
+		w: 15, h: 8,
+		enemySpeedFactor: 1.3,
+	},
+	// 8
+	{
+		w: 8, h: 16,
+		enemySpeedFactor: 1.4,
+	},
+	// 9
+	{
+		w: 15, h: 15,
+		enemySpeedFactor: 1.5,
+	}
+];
+
+
--- src/main.js
+++ src/main.js
@@ -7,8 +7,16 @@ import 'expose-loader?PIXI!phaser-ce/build/custom/pixi.js';
 import 'expose-loader?p2!phaser-ce/build/custom/p2.js';
 import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
 import { Grid, recursiveBackTracker, addDoors } from './maze';
-import { GAME_SCALE, WALL_TILE, KEY_TILE, KEY2_TILE, KEY3_TILE, START_TILE, EMPTY_TILE, GOAL_TILE, ENEMY_TILE, TILE_WIDTH, TILE_HEIGHT, NUM_TILES, BODY_H, BODY_LEFT, BODY_TOP, BODY_W } from './constants';
+import { 
+	GAME_SCALE, WALL_TILE, 
+	KEY_TILE, KEY2_TILE, KEY3_TILE, 
+	START_TILE, EMPTY_TILE, GOAL_TILE, ENEMY_TILE, 
+	TILE_WIDTH, TILE_HEIGHT, NUM_TILES, BODY_H, BODY_LEFT, BODY_TOP, BODY_W 
+} from './constants';
+
 import MenuState from './MenuState';
+import { levelData } from './level';
+
 class Game extends Phaser.Game {
 	
 	constructor() {
@@ -53,10 +61,17 @@ class GameState {
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 		this.game.physics.arcade.gravity.y = 0;
 
-		const grid = new Grid(10, 10);
+		this.currentLevel = 0;
+		this.levelConfig = levelData[this.currentLevel]; 
+		this.initLevel();
+		this.cursors = this.game.input.keyboard.createCursorKeys();
+	}
+
+	initLevel() {
+		const grid = new Grid(this.levelConfig.w, this.levelConfig.h);
 		recursiveBackTracker(grid);
 		addDoors(grid);
-	
+
 		// create a tilemap
 		this.map = this.game.add.tilemap();
 		this.map.setTileSize(TILE_WIDTH, TILE_HEIGHT);
@@ -75,7 +90,6 @@ class GameState {
 		this.keys = this.game.add.group();
 
 		let playerTile;
-		let goal;
 
 		// extract player, target and and enemy locations
 		for (let x = 0; x < map.width; ++x) {
@@ -93,7 +107,7 @@ class GameState {
 					case EMPTY_TILE: // open area
 						break;
 					case GOAL_TILE: // goal
-						goal = tile;
+						this.goal = tile;
 						break;
 					case ENEMY_TILE: // enemy
 						break;
@@ -119,6 +133,7 @@ class GameState {
 
 		this.keys.scale.setTo(GAME_SCALE);
 		this.map.setCollision(WALL_TILE, true, this.l1);
+		this.map.setCollision(GOAL_TILE, true, this.l1);
 
 		// this.map.createFromObjects('Tile Layer 1', KEY_TILE, SPRITESHEET, KEY_TILE, true, false, this.keys);
 		
@@ -144,15 +159,17 @@ class GameState {
 		this.player.body.setSize(BODY_W, BODY_H, BODY_LEFT, BODY_TOP);
 
 		this.game.camera.follow(this.player);
+	}
 
-		this.cursors = this.game.input.keyboard.createCursorKeys();
+	clearLevel() {
+		this.game.world.removeAll();
+	}
 
-		//this.player = player;
-
-		// const text = "- phaser -\n with a sprinkle of \n pixi dust.";
-		// const style = { font: "65px Arial", fill: "#ff0044", align: "center" };
-	
-		/* const t = */ // this.add.text(this.world.centerX - 300, 0, text, style);	
+	nextLevel() {
+		this.clearLevel();
+		this.currentLevel += 1;
+		this.levelConfig = levelData[this.currentLevel]; 
+		this.initLevel(); // initialize next level again
 	}
 
 	update() {
@@ -245,6 +262,10 @@ class GameState {
 	playerHitsWall(player, wall) {
 		player.body.velocity.x = 0;
 		player.body.velocity.y = 0;
+
+		if (wall == this.goal) {
+			this.nextLevel();
+		}
 	}
 
 	playerFindsKey(player, key) {
