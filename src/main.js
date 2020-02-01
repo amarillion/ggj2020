8a371dc81b05fd0a90211b61e69dfa62e3554930
Kerem Gokhan <kerem.gokhan@booking.com>
2020-02-01 11:53:31 +0100

    Add cursor movement

--- src/main.js
+++ src/main.js
@@ -23,6 +23,9 @@ class Game extends Phaser.Game {
 }
 
 const TILES_IMG = "sprites1";
+const SPRITESHEET = "sprites2";
+
+const UNIT = 320; // size of the square tiles in pixels
 
 class GameState {
 	
@@ -33,13 +36,18 @@ class GameState {
 
 	preload() {		
 		this.load.image(TILES_IMG, "assets/placeholder-sprites.png");
+		this.load.spritesheet(SPRITESHEET, "assets/placeholder-sprites.png", 8, 8, 63);
 		this.load.tilemap("level", "assets/placeholder-level.json", null, Phaser.Tilemap.TILED_JSON);
+		this.cursors = this.game.input.keyboard.createCursorKeys();
 	}
 
 	// called everytime state is entered
 	create () {
 		console.log("GameState.create");
 		this.game.stage.smoothed = false; // disable antialiasing
+
+		this.game.physics.startSystem(Phaser.Physics.ARCADE);
+		this.game.physics.arcade.gravity.y = 0;
 
 		// create a tilemap
 		this.map = this.game.add.tilemap("level");
@@ -52,7 +60,7 @@ class GameState {
 
 		const map = this.map;
 
-		let player;
+		let playerTile;
 		let goal;
 
 		// extract player, target and and enemy locations
@@ -62,9 +70,10 @@ class GameState {
 				const index = tile && tile.index;
 				switch (index) {
 				case 1: // wall
+					this.map.setCollision(index, true, this.l1);
 					break;
 				case 2: // player
-					player = tile;
+					playerTile = tile;
 					break;
 				case 7: // open area
 					break;
@@ -79,12 +88,53 @@ class GameState {
 			}
 		}
 		
-		console.log ({player, goal});
+		this.player = this.game.add.sprite(100, 100, SPRITESHEET, 2);
+		
+		this.player.anchor.set(0.5);
+		this.game.physics.arcade.enable(this.player);
+		
+		//  This adjusts the collision body size.
+		this.player.body.setSize(32, 32, 0, 0);
 
-		const text = "- phaser -\n with a sprinkle of \n pixi dust.";
-		const style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+		this.game.camera.follow(this.player);
+
+		this.cursors = this.game.input.keyboard.createCursorKeys();
+		//console.log ({this.player, goal});
+
+		//this.player = player;
+
+		// const text = "- phaser -\n with a sprinkle of \n pixi dust.";
+		// const style = { font: "65px Arial", fill: "#ff0044", align: "center" };
 	
-		/* const t = */ this.add.text(this.world.centerX - 300, 0, text, style);	
+		/* const t = */ // this.add.text(this.world.centerX - 300, 0, text, style);	
+	}
+
+
+	update() {
+		this.player.body.velocity.x = 0;
+		this.player.body.velocity.y = 0;
+		
+		//this.game.physics.arcade.collide(this.player, this.l1);
+		// console.log ("Old position x: " + this.player.x + ", y: " + this.player.y);
+
+		if (this.cursors.left.isDown)
+		{
+			this.player.body.velocity.x = -UNIT;
+		}
+		else if (this.cursors.right.isDown)
+		{
+			this.player.body.velocity.x = UNIT;
+		}
+
+		if (this.cursors.up.isDown)
+		{
+			this.player.body.velocity.y = -UNIT;
+		}
+		else if (this.cursors.down.isDown)
+		{
+			this.player.body.velocity.y = UNIT;
+		}
+
 	}
 }
 
